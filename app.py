2437ba49044f95149fdac4f83411b6607ff02f1b
Faris Wafda <111740313+Wafxd@users.noreply.github.com>
2026-04-15 16:08:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from scipy.signal import argrelextrema
 
 app = Flask(__name__)
 
-def analyze_stock(ticker, period="6mo"):
+def analyze_stock(ticker, period="2y"): # <- Ubah jadi 2y atau max
     # Download data
     df = yf.download(ticker, period=period, progress=False)
     if df.empty:
